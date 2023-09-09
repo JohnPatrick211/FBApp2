57ae1dc307d42b03f3679dfd2b2fd91e3ea965fd
Blankz <alvezwynsjohn@gmail.com>
2023-09-09 20:13:59 +0800

    Try redirect 4

--- app/Http/Controllers/PaymentCtr.php
+++ app/Http/Controllers/PaymentCtr.php
@@ -177,7 +177,7 @@ class PaymentCtr extends Controller
         ]);
 
         session()->forget('source');
-        return redirect('/tenant-payment')->with('success', 'Profile updated!')->send();
+        return redirect('/tenant-payment')->with('success', 'Your Payment Transaction is Successfully Created')->send();
     }
 }
 
