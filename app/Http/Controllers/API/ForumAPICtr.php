0ee76d0e9f316b6de0e2e2642488641bff2615c3
Blankz <alvezwynsjohn@gmail.com>
2023-09-09 20:50:08 +0800

    Update Query in Forum API

--- app/Http/Controllers/API/ForumAPICtr.php
+++ app/Http/Controllers/API/ForumAPICtr.php
@@ -15,7 +15,12 @@ use Illuminate\Support\Facades\DB;
 class ForumAPICtr extends Controller
 {
     public function getForum(Request $request){
-        $forum = DB::table('tbl_forum AS BR')
+
+        $id = $request->id;
+
+        if($id == 'ALL')
+        {
+            $forum = DB::table('tbl_forum AS BR')
             ->select('BR.*', 'tbl_tenant.*','tbl_employee.*','tbl_admin.*','BR.id AS forum_id', 'tbl_user.*','tbl_user.user_role AS role'
             ,'tbl_employee.fname AS emp_fname', 'tbl_employee.mname AS emp_mname', 'tbl_employee.lname AS emp_lname'
             ,'tbl_tenant.fname AS tenant_fname','tbl_tenant.mname AS tenant_mname', 'tbl_tenant.lname AS tenant_lname', 'BR.created_at AS created')
@@ -28,6 +33,24 @@ class ForumAPICtr extends Controller
             return response()->json([
                 'success' => true,
                 'forum' => $forum
-            ]);    
+            ]);
+        }
+        else{
+            $forum = DB::table('tbl_forum AS BR')
+            ->select('BR.*', 'tbl_tenant.*','tbl_employee.*','tbl_admin.*','BR.id AS forum_id', 'tbl_user.*','tbl_user.user_role AS role'
+            ,'tbl_employee.fname AS emp_fname', 'tbl_employee.mname AS emp_mname', 'tbl_employee.lname AS emp_lname'
+            ,'tbl_tenant.fname AS tenant_fname','tbl_tenant.mname AS tenant_mname', 'tbl_tenant.lname AS tenant_lname', 'BR.created_at AS created')
+            ->leftJoin('tbl_tenant', 'BR.author_id', '=', 'tbl_tenant.tenant_id')
+            ->leftJoin('tbl_employee', 'BR.author_id', '=', 'tbl_employee.emp_id')
+            ->leftJoin('tbl_admin', 'BR.author_id', '=', 'tbl_admin.admin_id')
+            ->leftJoin('tbl_user', 'BR.author_id', '=', 'tbl_user.id')
+            ->where('BR.author_id', $id)
+            ->get();
+
+            return response()->json([
+                'success' => true,
+                'forum' => $forum
+            ]);
+        }    
     }
 }
