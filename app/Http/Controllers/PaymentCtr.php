627c49d72ed85bebdefe042a57b7005b24d8f661
Blankz <alvezwynsjohn@gmail.com>
2023-09-09 20:10:19 +0800

    Try redirect

--- app/Http/Controllers/PaymentCtr.php
+++ app/Http/Controllers/PaymentCtr.php
@@ -177,7 +177,7 @@ class PaymentCtr extends Controller
         ]);
 
         session()->forget('source');
-        return redirect('/tenant-payment')->send();
+        return redirect('/tenant-payment')->with('status', 'Profile updated!');
     }
 }
 
