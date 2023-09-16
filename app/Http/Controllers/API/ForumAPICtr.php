e5cc7f903efc59743394fffd433c00277ae40225
Blankz <alvezwynsjohn@gmail.com>
2023-09-16 21:09:45 +0800

    Update getForum API

--- app/Http/Controllers/API/ForumAPICtr.php
+++ app/Http/Controllers/API/ForumAPICtr.php
@@ -23,7 +23,8 @@ class ForumAPICtr extends Controller
             $forum = DB::table('tbl_forum AS BR')
             ->select('BR.*', 'tbl_tenant.*','tbl_employee.*','tbl_admin.*','BR.id AS forum_id', 'tbl_user.*','tbl_user.user_role AS role'
             ,'tbl_employee.fname AS emp_fname', 'tbl_employee.mname AS emp_mname', 'tbl_employee.lname AS emp_lname'
-            ,'tbl_tenant.fname AS tenant_fname','tbl_tenant.mname AS tenant_mname', 'tbl_tenant.lname AS tenant_lname', 'BR.created_at AS created')
+            ,'tbl_tenant.fname AS tenant_fname','tbl_tenant.mname AS tenant_mname', 'tbl_tenant.lname AS tenant_lname', 'BR.created_at AS created'
+            ,'tbl_employee.profile_pic AS emp_profile_pic','tbl_tenant.profile_pic AS tenant_profile_pic')
             ->leftJoin('tbl_tenant', 'BR.author_id', '=', 'tbl_tenant.tenant_id')
             ->leftJoin('tbl_employee', 'BR.author_id', '=', 'tbl_employee.emp_id')
             ->leftJoin('tbl_admin', 'BR.author_id', '=', 'tbl_admin.admin_id')
