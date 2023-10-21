1bec1b29a2333647974979655750543dc71b93bb
Blankz <alvezwynsjohn@gmail.com>
2023-10-21 20:15:03 +0800

    Add Exist Validation

--- app/Http/Controllers/API/RegisterAPICtr.php
+++ app/Http/Controllers/API/RegisterAPICtr.php
@@ -18,54 +18,68 @@ class RegisterAPICtr extends Controller
 
         $user = new Login();
 
-        $user->user_role =  'Tenant';
-        $user->username =  $request->username;
-        $user->password =  Hash::make($request->password);
-        if ($request->profilepic) {
-            $file_path = time().'.jpg';
-            file_put_contents(public_path() . '/images/profile_pic/'.$file_path,base64_decode($request->profilepic));
-            $profile_pic = $file_path;
+        $exist = DB::table('tbl_user')
+        ->select('tbl_user.*','tbl_tenant.*')
+        ->leftJoin('tbl_tenant', 'tbl_user.id', '=', 'tbl_tenant.tenant_id')
+        ->where('tbl_tenant.email','=',  $request->email)
+        ->first();
+
+        if($exist){
+            $user->user_role =  'Tenant';
+            $user->username =  $request->username;
+            $user->password =  Hash::make($request->password);
+            if ($request->profilepic) {
+                $file_path = time().'.jpg';
+                file_put_contents(public_path() . '/images/profile_pic/'.$file_path,base64_decode($request->profilepic));
+                $profile_pic = $file_path;
+            }
+    
+            $user->save();
+    
+            $id = $user->id;
+    
+    
+            DB::table('tbl_tenant')
+                ->insert([
+                'tenant_id' => $id,
+                'room_id' => $request->room,
+                'fname' => $request->fname,
+                'mname' => $request->mname,
+                'lname' => $request->lname,
+                'email' => $request->email,
+                'address' => $request->address,
+                'gender' => $request->gender,
+                'civilstatus' => $request->civilstatus,
+                'age' => $request->age,
+                'phone' => $request->phone,
+                'birthdate' => $request->birthdate,
+                'date_of_occupancy' => $request->dateofoccupancy,
+                'contract_start' => $request->contractstart,
+                'contract_end' => $request->contractend,
+                'profile_pic' => $profile_pic,
+                'status' => '2',
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+                ]);
+    
+                $result =  DB::table('tbl_user')
+                ->select('tbl_user.*','tbl_tenant.*')
+                ->leftJoin('tbl_tenant', 'tbl_user.id', '=', 'tbl_tenant.tenant_id')
+                ->where('tbl_user.id','=', $id)
+                ->where('tbl_tenant.tenant_id','=',  $id)
+                ->first();
+    
+                return response()->json([
+                    'success' => true,
+                    'message' => 'For Verification',
+                    'result' => $result
+                ]);
         }
-
-        $user->save();
-
-        $id = $user->id;
-
-
-        DB::table('tbl_tenant')
-            ->insert([
-            'tenant_id' => $id,
-            'room_id' => $request->room,
-            'fname' => $request->fname,
-            'mname' => $request->mname,
-            'lname' => $request->lname,
-            'email' => $request->email,
-            'address' => $request->address,
-            'gender' => $request->gender,
-            'civilstatus' => $request->civilstatus,
-            'age' => $request->age,
-            'phone' => $request->phone,
-            'birthdate' => $request->birthdate,
-            'date_of_occupancy' => $request->dateofoccupancy,
-            'contract_start' => $request->contractstart,
-            'contract_end' => $request->contractend,
-            'profile_pic' => $profile_pic,
-            'status' => '2',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now(),
-            ]);
-
-            $result =  DB::table('tbl_user')
-            ->select('tbl_user.*','tbl_tenant.*')
-            ->leftJoin('tbl_tenant', 'tbl_user.id', '=', 'tbl_tenant.tenant_id')
-            ->where('tbl_user.id','=', $id)
-            ->where('tbl_tenant.tenant_id','=',  $id)
-            ->first();
-
+        else{
             return response()->json([
-                'success' => true,
-                'message' => 'Register Successfully',
-                'result' => $result
+                'success' => false,
+                'message' => 'User Already Exist'
             ]);
+        }
     }
 }
