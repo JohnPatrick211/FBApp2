aca3a706d04c6dc083c3f24a1dae81185b66be9a
Blankz <alvezwynsjohn@gmail.com>
2023-11-08 19:43:41 +0800

    test 24

--- app/Http/Controllers/API/EditTenantAPICtr.php
+++ app/Http/Controllers/API/EditTenantAPICtr.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class EditTenantAPICtr extends Controller
 {
