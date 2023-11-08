6e0c903dd2c95f08893214c785823c73bbb52a6c
Blankz <alvezwynsjohn@gmail.com>
2023-11-08 19:12:49 +0800

    test 14

--- resources/views/tenant-editprofile.blade.php
+++ resources/views/tenant-editprofile.blade.php
@@ -110,7 +110,7 @@
 
                         <div class="col-md-6 mb-3">
                             <label class="label-small">Username</label>
-                            <input type="password" class="form-control" name="editusername" id="editusername" value="{{$TenantInfo -> address}}" required>
+                            <input type="text" class="form-control" name="editusername" id="editusername" value="{{$TenantInfo -> address}}" required>
                         </div>
 
                         <div class="col-md-6 mb-3">
